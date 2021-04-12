1553d08f55b3be70f916d929b8a88353f7c5843b
trki293 <59773039+trki293@users.noreply.github.com>
2021-04-12 10:12:49 +0200

    feat [#3.6] : Creating dermatologist and get all dermatologists - backend

---
+++ back/src/main/java/com/isa/pharmacies_system/DTO/DermatologistNewDTO.java
@@ -0,0 +1,90 @@
+package com.isa.pharmacies_system.DTO;
+
+public class DermatologistNewDTO {
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String streetName;
+	private String streetNumber;
+	private String city;
+	private String country;
+	private String phoneNumber;
+	private String password;
+
+	public DermatologistNewDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
