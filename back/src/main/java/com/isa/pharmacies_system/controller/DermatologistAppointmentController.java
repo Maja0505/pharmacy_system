441352966698860045b0514ceb43e3f92c564a0d
Nemanja Jevtic <njevtic998@gmail.com>
2021-05-21 21:15:33 +0200

    feat[#3.30] : Search future examinations for dermatologist and pharmacist

--- back/src/main/java/com/isa/pharmacies_system/controller/DermatologistAppointmentController.java
+++ back/src/main/java/com/isa/pharmacies_system/controller/DermatologistAppointmentController.java
@@ -206,6 +206,30 @@ public class DermatologistAppointmentController {
         }
     }
 
+    //Nemanja
+    @GetMapping("/searchAllFutureReservedByPatient/{dermatologistId}/{firstName}/{lastName}")
+    public ResponseEntity<List<DermatologistAppointmentDTO>> searchFutureReservedAppointmentsByPatientFirstAndLastName(@PathVariable Long dermatologistId, @PathVariable String firstName,@PathVariable String lastName){
+        try {
+            List<DermatologistAppointment> dermatologistAppointmentList = dermatologistAppointmentService.searchAllFutureReservedByPatientFirstAndLastName(dermatologistId,firstName,lastName);
+            if(!dermatologistAppointmentList.isEmpty()){
+                List<DermatologistAppointmentDTO> dermatologistAppointmentDTOList = dermatologistAppointmentConverter.convertListOfDermatologistAppointmentToDermatologistAppointmentDTOS(dermatologistAppointmentList);
+                return new ResponseEntity<>(dermatologistAppointmentDTOList,HttpStatus.OK);
+            }
+            return new ResponseEntity<>(null,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
+    }
 
+    //Nemanja
+    @GetMapping("/allFutureReserveByDermatologist/{dermatologistId}")
+    public ResponseEntity<List<DermatologistAppointmentDTO>> getAllFutureReservedDermatologistAppointmentByDermatologist(@PathVariable Long dermatologistId){
+        try {
+            List<DermatologistAppointment> dermatologistAppointmentList = dermatologistAppointmentService.findAllFutureReservedDermatologistAppointmentByDermatologist(dermatologistId);
+            return new ResponseEntity<>(dermatologistAppointmentConverter.convertListOfDermatologistAppointmentToDermatologistAppointmentDTOS(dermatologistAppointmentList),HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
